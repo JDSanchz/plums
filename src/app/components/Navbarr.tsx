d53d997abfa6fbf10c61ccf607b20f4f4e372faa
JD Sanchez <67755598+JDSanchz@users.noreply.github.com>
2024-02-09 13:52:09 -0700

    Back to ENV + Sidebar Rendering from DB

--- src/app/components/Navbarr.tsx
+++ src/app/components/Navbarr.tsx
@@ -1,13 +1,30 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import topic from "../components/topics.json";
 import Topic from "../models/Topic";
 
 const Navbar = () => {
   const [isMenuVisible, setIsMenuVisible] = useState<Boolean>(false); // Menu visible by default
   const [isInput, setIsInput] = useState<Boolean>(false);
-  const [topics, setTopics] = useState<Topic[]>(topic);
+  const [topics, setTopics] = useState([]);
   const [newTopic, setNewTopic] = useState<string>("");
+
+  useEffect(() => {
+    const fetchTopics = async () => {
+      try {
+        const response = await fetch('/api/topics');
+        if (!response.ok) {
+          throw new Error(`Error: ${response.status}`);
+        }
+        const data = await response.json();
+        setTopics(data);
+      } catch (error) {
+        console.error('Failed to fetch topics:', error);
+      }
+    };
+
+    fetchTopics();
+  }, []);
+
 
   // Adjust visibility based on screen width
   useEffect(() => {
@@ -99,42 +116,18 @@ const Navbar = () => {
             </a>
             {/* Expandable Topics section */}
             <div className="p-4">
-              My Topics
-              <div className="mt-2">
-
-                {topics?.map((topic, key) => {
-                  return (
-                    <a
-                      key={key}
-                      href="#"
-                      className="mt-1 block  flex gap-2 p-1 pl-2 text-sm hover:bg-purple-100"
-                    >
-                      <div>
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          className="icon icon-tabler icon-tabler-library"
-                          width="18"
-                          height="18"
-                          viewBox="0 0 24 24"
-                          stroke-width="2"
-                          stroke="currentColor"
-                          fill="none"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                        >
-                          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                          <path d="M7 3m0 2.667a2.667 2.667 0 0 1 2.667 -2.667h8.666a2.667 2.667 0 0 1 2.667 2.667v8.666a2.667 2.667 0 0 1 -2.667 2.667h-8.666a2.667 2.667 0 0 1 -2.667 -2.667z" />
-                          <path d="M4.012 7.26a2.005 2.005 0 0 0 -1.012 1.737v10c0 1.1 .9 2 2 2h10c.75 0 1.158 -.385 1.5 -1" />
-                          <path d="M11 7h5" />
-                          <path d="M11 10h6" />
-                          <path d="M11 13h3" />
-                        </svg>
-                      </div>
-                      <p className="truncate">{topic.name}</p>
-                    </a>
-                  );
-                })}
-
+        My Topics
+        <div className="mt-2">
+          {topics.map((topic) => (
+            <a
+              key={topic.id}
+              href="#"
+              className="mt-1 block flex gap-2 p-1 pl-2 text-sm hover:bg-purple-100"
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-article" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 4m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z" /><path d="M7 8h10" /><path d="M7 12h10" /><path d="M7 16h10" /></svg>
+              <p className="truncate">{topic.title}</p>
+            </a>
+          ))}
                 {isInput && (
                   <div className="flex items-center justify-between p-2">
                     <input

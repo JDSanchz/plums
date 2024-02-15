916f68dedf3782cb15092a0192cfada25ba8d797
josephkaku <samkaks18@gmail.com>
2024-02-15 16:29:21 -0700

    upload

--- src/app/topic/[id]/page.tsx
+++ src/app/topic/[id]/page.tsx
@@ -5,12 +5,14 @@ import topic from "../../components/topics.json";
 import NewLinkForm from "@/app/components/links/LinkForm";
 import { useState } from "react";
 import Notes from "@/app/components/Notes";
+import UploadImage from "@/app/components/UploadImage";
 export default function page() {
   return (
     <div>
       <h1>topic</h1>
       <Notes />
       <NewLinkForm/>
+      <UploadImage/>
     </div>
   );
 }

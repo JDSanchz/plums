7fc18403c9f975de2e8e677dc42e66cef3750f64
josephkaku <samkaks18@gmail.com>
2024-02-15 16:09:05 -0700

    upload image added

--- src/app/topic/[id]/page.tsx
+++ src/app/topic/[id]/page.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import Welcome from "../../components/Welcome";
 import TopicCard from "../../components/TopicCard";
-import topic from "../../components/topics.json";
+import topic from "../../components/topics.json"
+import UploadImage from "@/app/components/UploadImage";
+
 
 import Notes from "@/app/components/Notes";
 export default function page() {
@@ -9,6 +11,7 @@ export default function page() {
     <div>
       <h1>topic</h1>
       <Notes />
+      <UploadImage />
     </div>
   );
 }

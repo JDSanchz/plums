be12a8ec1d2b982e66d63d578702aa80ce5a72e8
JD Sanchez <67755598+JDSanchz@users.noreply.github.com>
2024-02-15 15:45:33 -0700

    NewLink Comp

--- src/app/topic/[id]/page.tsx
+++ src/app/topic/[id]/page.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import Welcome from "../../components/Welcome";
 import TopicCard from "../../components/TopicCard";
 import topic from "../../components/topics.json";
-
+import NewLinkForm from "@/app/components/links/LinkForm";
+import { useState } from "react";
 import Notes from "@/app/components/Notes";
 export default function page() {
   return (
     <div>
       <h1>topic</h1>
       <Notes />
+      <NewLinkForm/>
     </div>
   );
 }

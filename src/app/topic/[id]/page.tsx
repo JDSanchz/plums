0fdec11e60e26199b9056922605551b0427be238
josephkaku <samkaks18@gmail.com>
2024-02-15 17:11:06 -0700

    accepted two changes

--- src/app/topic/[id]/page.tsx
+++ src/app/topic/[id]/page.tsx
@@ -5,11 +5,8 @@ import topic from "../../components/topics.json";
 import NewLinkForm from "@/app/components/links/LinkForm";
 import { useState } from "react";
 import Notes from "@/app/components/Notes";
-<<<<<<< HEAD
 import AllTopicLinks from "@/app/components/links/AllTopicLinks";
-=======
 import UploadImage from "@/app/components/images/UploadImage";
->>>>>>> cd84c3b0e3361314c3871220e322f7cb090e0ddc
 export default function page() {
   return (
     <div>

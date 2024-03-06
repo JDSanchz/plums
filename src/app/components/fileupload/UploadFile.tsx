914691c4d4ad01d52cb9e6c63b4ea04e78f60aae
josephkaku <samkaks18@gmail.com>
2024-03-06 09:48:56 -0700

    changed image sizes

--- src/app/components/fileupload/UploadFile.tsx
+++ src/app/components/fileupload/UploadFile.tsx
@@ -72,11 +72,11 @@ const deleteFile = async (id: string) => {
                         <><li className='flex flex-row gap-10' key={file.id as string}>
                             <p>{file.title}</p>
                             {file.data && file.mimeType && file.mimeType.startsWith('image/')   ? (
-                                <Image src={file.imageUrl} alt={file.title} width={100} height={100} />
+                                <Image src={file.imageUrl} alt={file.title} width={50} height={50} />
                                 ) : (
                                 <div>
                                     {/* Display a generic icon for non-image file types */}
-                                    <svg width="100px" height="100px" viewBox="0 0 24.00 24.00" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M15 19L17 21L21 17M13 3H8.2C7.0799 3 6.51984 3 6.09202 3.21799C5.71569 3.40973 5.40973 3.71569 5.21799 4.09202C5 4.51984 5 5.0799 5 6.2V17.8C5 18.9201 5 19.4802 5.21799 19.908C5.40973 20.2843 5.71569 20.5903 6.09202 20.782C6.51984 21 7.0799 21 8.2 21H11.5M13 3L19 9M13 3V7.4C13 7.96005 13 8.24008 13.109 8.45399C13.2049 8.64215 13.3578 8.79513 13.546 8.89101C13.7599 9 14.0399 9 14.6 9H19M19 9V13.4M9 17H11.5M9 13H15M9 9H10" stroke="#000000" stroke-width="0.768" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+                                    <svg width="50px" height="50px" viewBox="0 0 24.00 24.00" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M15 19L17 21L21 17M13 3H8.2C7.0799 3 6.51984 3 6.09202 3.21799C5.71569 3.40973 5.40973 3.71569 5.21799 4.09202C5 4.51984 5 5.0799 5 6.2V17.8C5 18.9201 5 19.4802 5.21799 19.908C5.40973 20.2843 5.71569 20.5903 6.09202 20.782C6.51984 21 7.0799 21 8.2 21H11.5M13 3L19 9M13 3V7.4C13 7.96005 13 8.24008 13.109 8.45399C13.2049 8.64215 13.3578 8.79513 13.546 8.89101C13.7599 9 14.0399 9 14.6 9H19M19 9V13.4M9 17H11.5M9 13H15M9 9H10" stroke="#000000" stroke-width="0.768" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
                                     <div>{file.title}</div>
                                 </div>
                             )}

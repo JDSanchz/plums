9c7bba969d2363406f9130c589698c1d100d2a80
josephkaku <samkaks18@gmail.com>
2024-03-26 16:15:41 -0600

    link for recently opened topics

--- src/app/components/recentlyOpenedTopics.tsx
+++ src/app/components/recentlyOpenedTopics.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import TopicCard from "./TopicCard";
 import { useTopics } from "./contexts/TopicProvider";
 import { Topic } from "../models/Topic";
+import Link from "next/link";
 
 const RecentlyOpenedTopics: React.FC = () => {
   const { topics, count, setCount } = useTopics();
@@ -22,7 +23,9 @@ const RecentlyOpenedTopics: React.FC = () => {
       <h1 className="text-2xl font-bold">Recent Topics</h1>
       <div className="mt-8 flex flex-grow flex-wrap gap-4">
       {[...recentTopics]?.reverse().map((topic) => (
-        <TopicCard key={topic.id} topic={topic} />
+      <Link key={topic?.id} href={`/topic/${topic?.id}`} className="flex h-fit flex-grow cursor-pointer gap-3 truncate rounded border  shadow-sm md:w-fit">
+            <TopicCard topic={topic} />
+      </Link>
       ))}
 
       </div>

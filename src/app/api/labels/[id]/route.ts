09dd3ce3318efc000135169aa5203dacb9b95176
josephkaku <samkaks18@gmail.com>
2024-03-28 17:07:11 -0600

    edit label and edit attachment title done

--- src/app/api/labels/[id]/route.ts
+++ src/app/api/labels/[id]/route.ts
@@ -30,3 +30,37 @@ export async function DELETE(request: Request) {
             });
         }
     }
+
+
+export async function PUT(request: Request) {
+    try{
+        const url = new URL(request.url);
+        const pathSegments = url.pathname.split("/"); // This splits the path into segments
+        const labelId = pathSegments[pathSegments.length - 1]; // Gets the last segment, which should be the ID
+        const { title } = await request.json();
+
+        const updateLabel = await prisma.label.update({
+            where: {
+                id: labelId,
+            },
+            data: {
+                title: title,
+            },
+        });
+
+        return new Response(JSON.stringify(updateLabel), {
+            status: 200,
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+    } catch (e) {
+        console.error("Error updating label: ", e);
+        return new Response(JSON.stringify({ error: "Unable to update label" }), {
+        status: 500,
+        headers: {
+            "Content-Type": "application/json",
+        },
+        });
+    }
+}
--- src/app/topic/label/page.tsx
+++ src/app/topic/label/page.tsx
@@ -2,31 +2,16 @@
 
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
-import { lastAccessed } from "@/app/components/services/recents";
 import { useParams } from 'next/navigation';
 
 
 
 export default function LabelPage() {
-    const { id:currentTopicId } = useParams();
     const { id:currentLabelId } = useParams();
-    const [topics, setTopics] = useState<Topic[]>([]);
     const [Labels, setLabels] = useState<Label[]>([]);
     const [newLabel, setNewLabel] = useState('');
-    const [isEditing, setIsEditing] = useState(false);
-    const [editTitle, setEditTitle] = useState('');
-    const [selectedLabelId, setSelectedLabelId] = useState<Label | null>(null);
-
-
-    interface Topic {
-      id: string;
-      title: string;
-      parentId: string;
-      lastAccessed: string;
-      createdAt: string;
-      updatedAt: string;
-      children: Topic[];
-    }
+    const [editMode, setEditMode] = useState<string | null>(null);
+    const [editLabel, setEditLabel] = useState('');
 
     interface Label {
       id: string;
@@ -72,23 +57,27 @@ export default function LabelPage() {
         }
       };
 
-      const updateLabelTitle = async (labelId: any, newTitle: string) => {
-        if (!confirm("Are you sure you want to update this label's title?")) {
-          return;
-        }
-
+      const updateLabelTitle = async (id:string) => {
         try {
-          const response = await fetch(`/api/labels/${labelId}`, {
+          const response = await fetch(`/api/labels/${id}`, {
             method: "PUT",
             headers: {
               "Content-Type": "application/json",
             },
-            body: JSON.stringify({ title: newTitle }),
+            body: JSON.stringify({ title: editLabel }),
           });
           if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
           }
-          fetchLabels();
+          setEditMode(null);
+          setEditLabel('');
+          const updatedLabels = Labels.map((label) => {
+            if (label.id === id) {
+              return { ...label, title: editLabel };
+            }
+            return label;
+          });
+          setLabels(updatedLabels);
         } catch (error) {
           console.error("Failed to update label:", error);
         }
@@ -142,7 +131,6 @@ export default function LabelPage() {
             <div className="flex gap-1 border rounded p-1 pl-2 text-sm">
             <div
               className={`flex gap-2 p-1 pl-2 text-sm border hover:bg-purple-100 ${Label.id === currentLabelId ? 'bg-purple-300' : ''}`}
-              // onClick={() => handleTopicDelete(Label.id)}
               >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -163,7 +151,15 @@ export default function LabelPage() {
                 <path d="M11 10h6" />
                 <path d="M11 13h3" />
               </svg>
+              {editMode === Label.id ? (
+                <input
+                  type="text"
+                  value={editLabel}
+                  onChange={(e) => setEditLabel(e.target.value)}
+                />
+              ) : (
               <p className="truncate">{Label.title}</p>
+              )}
 
             </div> 
             <button 
@@ -187,8 +183,38 @@ export default function LabelPage() {
                 <path d="M12 10l4 4m0 -4l-4 4" />
                 </svg>
                 </button>
+                {editMode === Label.id ? (
+                  <button className="ml-4 px-2 py-1 bg-blue-500 text-white rounded-lg hover:bg-blue-600" onClick={()=> updateLabelTitle(Label.id)}>Update Label</button>)
+                :(
+                  <button className="ml-4 px-2 py-1 bg-blue-500 text-white rounded-lg hover:bg-blue-600" onClick={() => {setEditMode(Label.id); }}> Edit Label </button>
+                )}
+            {/* {editMode !== Label.id ? (
+              
+            <button 
+            className="ml-4 px-2 py-1 bg-red-500 text-white rounded-lg hover:bg-red-600" 
+            onClick={() => handleTopicDelete(Label.id)}
+            >
+            <svg  
+              xmlns="http://www.w3.org/2000/svg"  
+              width="24"  
+              height="24"  
+              viewBox="0 0 24 24"  
+              fill="none"  
+              stroke="currentColor"  
+              stroke-width="1.25"  
+              stroke-linecap="round"  
+              stroke-linejoin="round"  
+              className="icon icon-tabler icons-tabler-outline icon-tabler-backspace"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <path d="M20 6a1 1 0 0 1 1 1v10a1 1 0 0 1 -1 1h-11l-5 -5a1.5 1.5 0 0 1 0 -2l5 -5z" />
+                <path d="M12 10l4 4m0 -4l-4 4" />
+                </svg>
+                </button>
+            ) ? (
+            <button className="ml-4 px-2 py-1 bg-blue-500 text-white rounded-lg hover:bg-blue-600" onClick={() => updateLabelTitle(Label.id) }> Edit Label </button> 
+            ) : " " :""} */}
 
-                <button className="ml-4 px-2 py-1 bg-blue-500 text-white rounded-lg hover:bg-blue-600"> Edit Label </button>
             </div>
             </Link>
         </React.Fragment>
@@ -213,149 +239,3 @@ export default function LabelPage() {
     </div>
     )}
     
-// import React, { useEffect, useState } from "react";
-// import Link from "next/link";
-// import { lastAccessed } from "@/app/components/services/recents";
-// import { useParams } from 'next/navigation';
-
-// interface Label {
-//   id: string;
-//   title: string;
-//   createdAt: Date;
-//   updatedAt: Date;
-//   topicId: number;
-// }
-
-// export default function LabelPage() {
-//   const { id: currentTopicId } = useParams();
-//   const [labels, setLabels] = useState<Label[]>([]);
-//   const [newLabel, setNewLabel] = useState('');
-//   const [isEditing, setIsEditing] = useState(false);
-//   const [editTitle, setEditTitle] = useState('');
-//   const [selectedLabelId, setSelectedLabelId] = useState<string | null>(null);
-
-//   useEffect(() => {
-//     fetchLabels();
-//   }, []);
-
-//   // Fetch all labels
-//   const fetchLabels = async () => {
-//     try {
-//       const response = await fetch("/api/labels");
-//       if (!response.ok) {
-//         throw new Error(`Error: ${response.status}`);
-//       }
-//       const data = await response.json();
-//       const sortedData = data.sort((a: { createdAt: string | number | Date; }, b: { createdAt: string | number | Date; }) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime());
-//       setLabels(sortedData);
-//     } catch (error) {
-//       console.error("Failed to fetch labels:", error);
-//     }
-//   };
-
-//   const handleTopicDelete = async (labelId: string) => {
-//     // Delete the label when the delete button is clicked
-//     try {
-//       const response = await fetch(`/api/labels/${labelId}`, {
-//         method: "DELETE",
-//       });
-//       if (!response.ok) {
-//         throw new Error(`HTTP error! status: ${response.status}`);
-//       }
-//       fetchLabels();
-//     } catch (error) {
-//       console.error("Failed to delete label:", error);
-//     }
-//   };
-
-//   const handleEditLabel = (labelId: string, currentTitle: string) => {
-//     // Set the state to start editing and set the current title
-//     setSelectedLabelId(labelId);
-//     setEditTitle(currentTitle);
-//     setIsEditing(true);
-//   };
-
-//   const updateLabelTitle = async (labelId: string, newTitle: string) => {
-//     if (!confirm("Are you sure you want to update this label's title?")) {
-//       return;
-//     }
-
-//     try {
-//       const response = await fetch(`/api/labels/${labelId}`, {
-//         method: "PUT",
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify({ title: newTitle }),
-//       });
-//       if (!response.ok) {
-//         throw new Error(`HTTP error! status: ${response.status}`);
-//       }
-//       fetchLabels();
-//       setIsEditing(false);
-//     } catch (error) {
-//       console.error("Failed to update label:", error);
-//     }
-//   };
-
-//   const createNewLabel = async (newLabel: string) => {
-//     try {
-//       const response = await fetch("/api/labels", {
-//         method: "POST",
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify({
-//           title: newLabel
-//         })
-//       });
-//       if (!response.ok) {
-//         throw new Error(`HTTP error! status: ${response.status}`);
-//       }
-//       fetchLabels();
-//     } catch (error) {
-//       console.error("Failed to create label:", error);
-//     }
-//   };
-
-//   return (
-//     <div className="p-4">
-//       <p className="font-semibold">Labels currently logged</p>
-//       {/* Display labels */}
-//       <div className="mt-2 max-h-60 overflow-auto flex flex-wrap gap-2">
-//         {labels.map((label) => (
-//           <div key={label.id} className="flex gap-1 border rounded p-1 pl-2 text-sm">
-//             <p className="truncate">{label.title}</p>
-//             {isEditing && selectedLabelId === label.id ? (
-//               <>
-//                 <input
-//                   type="text"
-//                   value={editTitle}
-//                   onChange={(e) => setEditTitle(e.target.value)}
-//                 />
-//                 <button onClick={() => updateLabelTitle(label.id, editTitle)}>Save</button>
-//               </>
-//             ) : (
-//               <>
-//                 <button onClick={() => handleEditLabel(label.id, label.title)}>Edit</button>
-//                 <button onClick={() => handleTopicDelete(label.id)}>Delete</button>
-//               </>
-//             )}
-//           </div>
-//         ))}
-//       </div>
-//       {/* Section to add labels */}
-//       <div className="mt-4">
-//         <p className="font-semibold">Create new Label</p>
-//         <input
-//           placeholder="Enter label name"
-//           value={newLabel}
-//           onChange={(e) => setNewLabel(e.target.value)}
-//           type="text"
-//           required
-//         />
-//         <button onClick={() => createNewLabel(newLabel)}>Add</button>
-//       </div>
-//     </div>
-//   );
-// }

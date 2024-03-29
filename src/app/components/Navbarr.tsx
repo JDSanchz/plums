c6f87cfdaa798bad40f4fe504fbc6f85dc65130c
JD Sanchez <67755598+JDSanchz@users.noreply.github.com>
2024-03-29 08:49:17 -0600

    better login and logout based on user

--- src/app/components/Navbarr.tsx
+++ src/app/components/Navbarr.tsx
@@ -260,11 +260,25 @@ const Navbar = () => {
             <div className="py-0 pl-4">
               <NewTopicInput newInput={setNewInput} />
             </div>
-            <a href="/api/auth/login" className="flex items-center p-4 hover:bg-gray-200">
-            <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-user mr-2"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" /><path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" /></svg>
-                Login
-            </a>
-            <a href="/api/auth/logout">Logout</a>
+            {user ? (
+        <a href="/api/auth/logout" className="flex items-center p-4 hover:bg-gray-200">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icon-tabler-user mr-2">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
+            <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+          </svg>
+          Logout
+        </a>
+      ) : (
+        <a href="/api/auth/login" className="flex items-center p-4 hover:bg-gray-200">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icon-tabler-user mr-2">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
+            <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+          </svg>
+          Login
+        </a>
+      )}
             <Link href={`/topic/label/`} className="flex items-center p-4 hover:bg-gray-200">
             <svg xmlns="http://www.w3.org/2000/svg" 
               className="icon icon-tabler icon-tabler-tags mr-2" 

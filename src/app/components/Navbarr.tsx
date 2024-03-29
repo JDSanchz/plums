894ba0bdbec82f800b51ef2a3130bb147f3f47ea
JD Sanchez <67755598+JDSanchz@users.noreply.github.com>
2024-03-29 08:39:33 -0600

    Hamburger

--- src/app/components/Navbarr.tsx
+++ src/app/components/Navbarr.tsx
@@ -152,7 +152,23 @@ const Navbar = () => {
           onClick={toggleMenuVisibility}
           className="ml-auto cursor-pointer border-none bg-transparent md:hidden"
         >
-          {/* SVG for the button */}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="icon icon-tabler icon-tabler-menu-2"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            strokeWidth="2"
+            stroke="currentColor"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M4 6l16 0" />
+            <path d="M4 12l16 0" />
+            <path d="M4 18l16 0" />
+          </svg>
         </button>
       </div>
       {/* Conditional rendering based on isMenuVisible */}

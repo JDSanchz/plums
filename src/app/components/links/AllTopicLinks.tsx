049d399379904cbd172fe3bc6338386adb880140
JD Sanchez <67755598+JDSanchz@users.noreply.github.com>
2024-04-04 15:08:51 -0600

    new key

--- src/app/components/links/AllTopicLinks.tsx
+++ src/app/components/links/AllTopicLinks.tsx
@@ -134,7 +134,7 @@ const AllTopicLinks: React.FC<AllTopicLinksProps> = ({ links, updateLinks }) => 
                     <p className="text-gray-600">{link.desc}</p>
                   </div>
                   <img src={`https://api.screenshotone.com/take?
-	access_key=lpKpD06g2RRL3g
+	access_key=Id53MuBcmnqVNA
 	&url=${link.url}`} alt={`Screenshot of ${link.title}`} className="w-20 h-20 object-cover md:mr-12 md:mb-0 md:w-52 md:h-24" />
                   <div className="flex flex-col ml-3">
                     <button className="text-blue-600 hover:text-blue-800" onClick={() => startEditing(link)}><svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-edit" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1" /><path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z" /><path d="M16 5l3 3" /></svg></button>
